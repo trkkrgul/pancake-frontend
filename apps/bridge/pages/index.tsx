c940594eef392ad12af33c5a58849c96e4a858e4
trkkrgul <109871858+trkkrgul@users.noreply.github.com>
2023-02-08 11:38:55 +0300

    d

--- apps/bridge/pages/index.tsx
+++ apps/bridge/pages/index.tsx
@@ -5,6 +5,7 @@ import styled, { useTheme } from 'styled-components'
 import { Flex, Text, Box } from '@pancakeswap/uikit'
 import { STARGATE_JS } from '../components/stargate/config'
 import { StargateWidget } from '../components/stargate'
+
 const Page = styled.div`
   height: 100%;
   display: flex;
